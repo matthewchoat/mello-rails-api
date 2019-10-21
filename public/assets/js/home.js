30a703410b30b2a71b0427e29165601d815dee78
Matthew Choat <matthewchoat@gmail.com>
2019-10-21 05:00:32 +0000

    create account, login, create, and show boards.

---
+++ public/assets/js/home.js
@@ -0,0 +1,107 @@
+const $submitButton = $('#submit');
+const $setLogin = $('#login');
+const $setSignUp = $('#signup');
+const $emailInput = $('#email');
+const $passwordInput = $('#password');
+const $message = $('#message');
+
+let authSetting = 'login';
+
+sendUserToBoards();
+
+function sendUserToBoards() {
+  if (localStorage.getItem('user')) {
+    location.replace('/boards');
+  }
+}
+
+function setAuth(setting) {
+  authSetting = setting;
+
+  if (authSetting === 'login') {
+    $setLogin.addClass('active');
+    $setSignUp.removeClass('active');
+    $submitButton.text('Log In');
+  } else {
+    $setSignUp.addClass('active');
+    $setLogin.removeClass('active');
+    $submitButton.text('Sign Up');
+  }
+}
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  let email = $emailInput.val().trim();
+  let password = $passwordInput.val().trim();
+
+  if (!email || !password) {
+    displayMessage('Email and password fields cannot be blank.', 'danger');
+    return;
+  }
+
+  $emailInput.val('');
+  $passwordInput.val('');
+
+  authenticateUser(email, password);
+}
+
+function displayMessage(message, type) {
+  $message.text(message).attr('class', type);
+}
+
+function handleSignupResponse(status) {
+  if (status === 'success') {
+    displayMessage('Registered successfully! You may now sign in.', 'success');
+    setAuth('login');
+  } else {
+    displayMessage(
+      'Something went wrong. A user with this account may already exist.',
+      'danger'
+    );
+  }
+}
+
+function handleLoginResponse(data, status, jqXHR) {
+  if (status === 'success') {
+    let jwt = jqXHR.getResponseHeader('authorization');
+    let user = JSON.stringify(data);
+
+    localStorage.setItem('authorization', jwt);
+    localStorage.setItem('user', user);
+    sendUserToBoards();
+  } else {
+    displayMessage('Invalid email or password.', 'danger');
+  }
+}
+
+function authenticateUser(email, password) {
+  $.ajax({
+    url: '/' + authSetting,
+    data: {
+      user: {
+        email,
+        password
+      }
+    },
+    method: 'POST'
+  })
+    .then(function(data, status, jqXHR) {
+      if (authSetting === 'signup') {
+        handleSignupResponse(status);
+      } else {
+        handleLoginResponse(data, status, jqXHR);
+      }
+    })
+    .catch(function(err) {
+      if (authSetting === 'signup') {
+        handleSignupResponse(err.statusText);
+      } else {
+        handleLoginResponse(err.statusText);
+      }
+    });
+}
+
+$setLogin.on('click', setAuth.bind(null, 'login'));
+$setSignUp.on('click', setAuth.bind(null, 'signup'));
+$submitButton.on('click', handleFormSubmit);
---
+++ public/assets/js/user-boards.js
@@ -0,0 +1,83 @@
+const $logoutButton = $('#logout');
+const $newBoardButton = $('#new-board');
+const $boardNameInput = $('#board-name');
+const $saveBoardButton = $('#save-board');
+const $boardsContainer = $('.boards');
+
+let user;
+
+init();
+
+function init() {
+  user = JSON.parse(localStorage.getItem('user'));
+
+  if (!user) {
+    localStorage.clear();
+    location.replace('/');
+    return;
+  }
+
+  $('.welcome h1').text('Welcome ' + user.email + '!');
+
+  getUserBoards();
+}
+
+function getUserBoards() {
+  $.ajax({
+    url: `/api/users/${user.id}`,
+    method: 'GET'
+  }).then(function(data) {
+    renderBoards(data.boards);
+  });
+}
+
+function renderBoards(boards) {
+  $boardsContainer.empty();
+
+  let $boardTiles = boards.map(function(board) {
+    let $boardTile = $('<a class="board-tile">')
+      .attr('href', `/boards/${board.id}`)
+      .text(board.name);
+
+    return $boardTile;
+  });
+
+  $boardsContainer.append($boardTiles);
+}
+
+function handleBoardCreate(event) {
+  event.preventDefault();
+
+  let boardName = $boardNameInput.val().trim();
+
+  $boardNameInput.val('');
+
+  if (!boardName) {
+    return;
+  }
+
+  $.ajax({
+    url: '/api/boards',
+    data: {
+      name: boardName
+    },
+    method: 'POST'
+  }).then(function() {
+    getUserBoards();
+    MicroModal.close('create-board');
+  });
+}
+
+function handleLogout() {
+  $.ajax({
+    url: '/logout',
+    method: 'DELETE'
+  }).then(function() {
+    localStorage.clear();
+    location.replace('/');
+  });
+}
+
+$logoutButton.on('click', handleLogout);
+$newBoardButton.on('click', MicroModal.show.bind(null, 'create-board'));
+$saveBoardButton.on('click', handleBoardCreate);
